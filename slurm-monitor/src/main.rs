1805b22032d2af44c90a0bbabd594292a72fa722
Johannes Blaschke <jpblaschke@lbl.gov>
2024-01-21 22:07:33 -0800

    table output

--- slurm-monitor/src/main.rs
+++ slurm-monitor/src/main.rs
@@ -2,7 +2,26 @@ mod cli;
 mod slurm;
 
 use crate::cli::{init, parse};
-use crate::slurm::check_slurm;
+use crate::slurm::{check_slurm, JobStats};
+
+use tabled::{Tabled, Table};
+
+#[derive(Tabled)]
+struct JobStatsRow<'a> {
+    name: &'a str,
+    running: u64,
+    waiting: u64
+}
+
+impl JobStats {
+    fn to_table<'a>(&'a self, name: &'a str) -> JobStatsRow {
+        JobStatsRow{
+            name: name,
+            running: self.running,
+            waiting: self.waiting
+        }
+    }
+}
 
 fn main() {
     let args = init();
@@ -12,7 +31,11 @@ fn main() {
 
     match check_slurm(&cli.name){
         Ok(job_stats) => {
-            println!("{}", job_stats);
+            let table = vec![
+                job_stats.to_table(&cli.name)
+            ];
+            let table_str = Table::new(table).to_string();
+            println!("{}", table_str);
         }
         Err(error) => {
             eprintln!("Error occurred: {}", error);
