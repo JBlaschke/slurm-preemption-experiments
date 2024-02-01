256e073bf2c134f48d3adfa6473611f567f11117
Johannes Blaschke <jpblaschke@lbl.gov>
2024-01-31 17:14:54 -0800

    add more exp

---
+++ experiments/pod_1b/preempting_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -A nstaff
+#SBATCH -C cpu
+#SBATCH -q regular
+#SBATCH -t 00:02:00
+#SBATCH --reservation=pod_preempt_3
+#SBATCH --job-name=patient_app
+
+echo "1"
+sleep 1m
